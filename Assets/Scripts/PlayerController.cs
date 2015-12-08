3c9f35b820cdaefe6aaeef6dbb8a42c0b536fb4a
Roman Bondarchuk <rmnbnd@gmail.com>
2015-12-08 22:38:03 +0200

    Add remove enemy object by click on space

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerController : MonoBehaviour {
+
+	private GameObject[] enemies;
+
+	// Use this for initialization
+	void Start () {
+		enemies = GameObject.FindGameObjectsWithTag("Enemy");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			foreach (GameObject enemy in enemies) {
+				Destroy(enemy);
+			}
+		}
+	}
+}
