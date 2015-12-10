18d9aa16e9c839fb76a888044cee641766c0f26d
Roman Bondarchuk <rmnbnd@gmail.com>
2015-12-10 23:46:01 +0200

    Select only one word enemy

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 public class PlayerController : MonoBehaviour {
 
 	private GameObject[] enemies;
+	private GameObject currentEnemy;
 
 	// Use this for initialization
 	void Start () {
@@ -13,18 +14,33 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		enemies = GameObject.FindGameObjectsWithTag("Enemy Text");
+
 		if (Input.anyKeyDown) {
+			if (currentEnemy != null) {
+				checkWord(currentEnemy);
+				return;
+			}
+			enemies = GameObject.FindGameObjectsWithTag("Enemy Text");
 			foreach (GameObject enemy in enemies) {
-				EnemyTextController enemyObject = enemy.GetComponent<EnemyTextController>();
-				if (enemyObject.word[0].ToString() == Input.inputString) {
-					if (enemyObject.word.Length == 1) {
-						Destroy(enemy.transform.parent.gameObject);
-					} else {
-						enemyObject.word = enemyObject.word.Substring(1, enemyObject.word.Length - 1);						
-					}
+				if (checkWord(enemy)) {
+					currentEnemy = enemy;
+					return;
 				}
 			}
 		}
+	}
+
+	private bool checkWord(GameObject enemy) {
+		bool isSelectWord = false;
+		EnemyTextController enemyObject = enemy.GetComponent<EnemyTextController>();
+		if (enemyObject.word[0].ToString() == Input.inputString) {
+			isSelectWord = true;
+			if (enemyObject.word.Length == 1) {
+				Destroy(enemy.transform.parent.gameObject);
+			} else {
+				enemyObject.word = enemyObject.word.Substring(1, enemyObject.word.Length - 1);						
+			}
+		}
+		return isSelectWord;
 	}
 }
